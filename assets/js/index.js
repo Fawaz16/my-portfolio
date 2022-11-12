182b46325b99930423101c4bd7b21452088e944f
Fawaz16 <99909801+Fawaz16@users.noreply.github.com>
2022-11-12 20:26:18 +0100

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+$(".bb-text").owlCarousel({
+    margin: 10,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+    stagepadding:20,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      650: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
+  
+  
+  
